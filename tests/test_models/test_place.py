8ad64498d21d9d183bdc413b20894cbfd8e22232
NecheHephzibah <neche.bless@gmail.com>
2024-07-31 11:29:13 -0700

    AirBnB

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
@@ -67,3 +68,4 @@ class test_Place(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
+
